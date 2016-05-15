0fc8491ae1d1dfb862c3b2d85ae69300692d27db
Bozhidar Hristov <b.hristov@vm5.eu>
2016-05-16 00:07:45 +0300

    typo

---
+++ PhpProcessor.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Incenteev\ParameterHandler;
+
+class PhpProcessor implements FileProcessorInterface
+{
+
+    public function dump($file, $values)
+    {
+        
+    }
+
+    public function parse($file)
+    {
+        
+    }
+
+    public function supports($file)
+    {
+        
+    }
+
+}
